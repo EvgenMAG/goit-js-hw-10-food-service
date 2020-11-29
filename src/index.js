f13aab2ec23dc47a7d1da2fa34170287a588f8d6
Evgeniy Chernyshenko <magdegaben@gmail.com>
2020-11-30 00:51:24 +0200

    adding themes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
-
 import './styles.css';
 import itemsTemplate from './templates/gallery-items.hbs';
 import menuList from './menu.json';
-import themSwitcher from './templates/page-theme.hbs'
+
+export const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
 const markUp = itemsTemplate(menuList);
-const galleryRef = document.querySelector('.js-menu');
-galleryRef.insertAdjacentHTML('beforeend', markUp)
 
-const switcher = document.querySelector('theme-switch__toggle')
-console.log(switcher);
-switcher.addEventListener()
+const galleryRef = document.querySelector('.js-menu');
+const switcher = document.querySelector('.theme-switch__toggle');
+const bodyRef = document.querySelector('body');
+
+galleryRef.insertAdjacentHTML('beforeend', markUp);
+
+switcher.addEventListener('change', handlingChangeThemes);
+
+const switcherStatus = localStorage.getItem('theme');
+
+if (switcherStatus === Theme.DARK) {
+  bodyRef.classList.add(Theme.DARK);
+  switcher.checked = true;
+}
+if (switcherStatus === Theme.LIGHT) {
+  bodyRef.classList.add(Theme.LIGHT);
+}
+
+function handlingChangeThemes() {
+  if (!switcher.checked) {
+    localStorage.setItem('theme', Theme.LIGHT);
+    bodyRef.classList.add(Theme.LIGHT);
+    bodyRef.classList.remove(Theme.DARK);
+  }
+  if (switcher.checked) {
+    localStorage.setItem('theme', Theme.DARK);
+    bodyRef.classList.add(Theme.DARK);
+    bodyRef.classList.remove(Theme.LIGHT);
+  }
+}
