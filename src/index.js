b395a8ec7d81ada3a3f3a468c958da9d9b4773f1
Evgeniy Chernyshenko <magdegaben@gmail.com>
2020-11-29 10:57:45 +0200

    adding markup from template_hsb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+
 import './styles.css';
+import itemsTemplate from './templates/gallery-items.hbs';
+import menuList from './menu.json';
+import themSwitcher from './templates/page-theme.hbs'
+
+const markUp = itemsTemplate(menuList);
+const galleryRef = document.querySelector('.js-menu');
+galleryRef.insertAdjacentHTML('beforeend', markUp)
+
+const switcher = document.querySelector('theme-switch__toggle')
+console.log(switcher);
+switcher.addEventListener()
